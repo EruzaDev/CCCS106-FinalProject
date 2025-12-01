021d86cfd2b3a48935d5c7d8281093822dfbb8db
jpcaldo2020 <jocaldo@my.cspc.edu.ph>
2025-12-01 20:55:43 +0800

    Initialize project structure and dependencies

---
+++ components/__init__.py
@@ -0,0 +1,8 @@
+# Components package
+from .top_taskbar import TopTaskbar
+from .sidebar import Sidebar
+from .post_container import PostContainer
+from .right_sidebar import RightSidebar
+from .notification_dropdown import NotificationDropdown
+from .post_card import PostCard
+from .post_creator import PostCreator
---
+++ pages/__init__.py
@@ -0,0 +1,4 @@
+# Pages package
+from .login_page import LoginPage
+from .signup_page import SignupPage
+from .home_page import HomePage
